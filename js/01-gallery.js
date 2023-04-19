9db01d297c5f00284d43e87a640b8edd43f39052
ginkosoul <ginkosoul@gmail.com>
2023-04-19 10:21:27 +0300

    added gallery basicLightbox and SimpleLightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,83 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import * as basicLightbox from './basicLightbox.min.js'
+// const basicLightbox = require('basiclightbox')
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryElement = document.querySelector('.gallery');
+// console.log(galleryElement);
+
+// let instance = basicLightbox.create(`
+//   <img src="" width="1280">
+// `);
+
+const instance = basicLightbox.create(`
+    <img src="" width="1280">
+`);
+
+// console.log(instance);
+
+const createGalleryElementMarkupFromObject = ({preview, original, description}) => `
+<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`;
+
+const createGalleryMarkupFromImages = galleryItems
+  .map(createGalleryElementMarkupFromObject)
+  .join(``);
+
+galleryElement.insertAdjacentHTML('afterbegin',createGalleryMarkupFromImages);
+// console.log(createGalleryMarkupFromImages)
+
+// galleryElement
+
+galleryElement.addEventListener('click', onClick)
+
+function onClick(event) {
+    
+    event.preventDefault();
+
+    const image = event.target.dataset.source;
+    // const imageLink = event.target.closest('.gallery__link');
+    // imageLink.preventDefault;
+    // console.log(imageLink)
+    changeImageModal(instance,image);
+
+    window.addEventListener('keydown', onImageClose)
+    
+    instance.show()
+}
+
+function onImageClose(event){
+    if (event.code === `Escape`) {
+        instance.close();
+        window.removeEventListener('keydown', onImageClose)
+    }
+    // console.log(event.code)
+}
+
+
+function changeImageModal(instance,url){
+
+  // instance.insertAdjacentHTML(`
+  // <img src="#" width="1280">
+  // `)
+
+  // instance.innerHTML = '';
+
+  // instance.appendChild()
+  // console.log(instance);
+
+  const modalElement = instance.element().querySelector('.basicLightbox__placeholder');
+  modalElement.innerHTML = `
+  <img src="${url}" width="1280">
+  `;
+  // console.log(modalElement);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// import SimpleLightbox from "simplelightbox"
 
 console.log(galleryItems);
+// console.log(SimpleLightbox);
+const galleryElement = document.querySelector('ul.gallery');
+
+const createGalleryElementMarkupFromObject = ({preview, original, description}) => `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+
+const createGalleryMarkup = galleryItems
+  .map(createGalleryElementMarkupFromObject)
+  .join(``);
+
+galleryElement.insertAdjacentHTML('afterbegin',createGalleryMarkup);
+
+galleryElement.addEventListener('click',onClick)
+
+const galleryLinks = galleryElement.querySelectorAll('a')
+// console.log(galleryLinks);
+
+let gallery = new SimpleLightbox(galleryLinks);
+
+gallery.on('show.simplelightbox', function () {
+	// Do something…
+});
+
+function onClick(event){
+    event.preventDefault();
+}
